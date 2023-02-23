6adc9e5e863de75e2b8ec07169db18015cd7f873
Davis-Okumu <davisokumu12@gmail.com>
2023-02-22 23:29:56 -0800

    Betty would be proud

---
+++ betty_checks.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * Main - Simple hello world
+ *
+ * Return: Always 0.
+ */
+int main ()
+{
+	char hello[] = 'h', 'e', 'l', 'l', 'w', 'o', 'r', 'l', 'd';
+
+	for(i = 0; i < hello[i]; i++)
+	{
+		_putchar(hello[i]);
+	}
+	return (0);
+}
