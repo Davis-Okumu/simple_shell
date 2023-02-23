a1d538974d128d325aed53e9b7a43aad6c39a93a
Davis-Okumu <davisokumu12@gmail.com>
2023-02-23 00:09:28 -0800

    Betty would be proud

--- betty_checks.c
+++ betty_checks.c
@@ -2,12 +2,28 @@
 #include <stdlib.h>
 
 /**
- * Main - Simple hello world
+ * sum - Finds the sum of two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: void
+ */
+void sum(int a, int b)
+{
+        int c, d, summ;
+
+        summ = a + b;
+        printf("The sum is: %d", summ);
+
+}
+
+/**
+ * Main - Main function
  *
  * Return: Always 0.
  */
 int main ()
 {
-	printf("Hello from Kenya");
-	return (0);
+        sum(4,5);
+
+        return (0);
 }
